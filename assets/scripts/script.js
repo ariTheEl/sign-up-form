5a8c9d23529b8bb51b1cf2e4724d7db323eaf27f
Navwojah-Ariko_Eloho <arikoeloho1996@gmail.com>
2022-09-17 11:07:20 +0100

    feat: add an error class to password input

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,24 @@
 const submit = document.querySelector('#submit');
-
+password = document.querySelector('#password');
+confirm = document.querySelector('#confirm-password');
 function compare(e) {
-    password = document.querySelector('#password');
-    confirm = document.querySelector('#confirm-password');
-
     if(password.value === confirm.value){
     }
-
     else{
         e.preventDefault();
     }
-    console.log(e.defaultPrevented);
 }
 
+function errorClass(){
+    if(password.value === confirm.value){
+        password.classList.remove('error');
+        confirm.classList.remove('error');
+    }
+    else{
+        password.classList.add('error');
+        confirm.classList.add('error');
+    }
+}
+password.addEventListener('input', errorClass);
+confirm.addEventListener('input', errorClass);
 submit.addEventListener('click', compare);
